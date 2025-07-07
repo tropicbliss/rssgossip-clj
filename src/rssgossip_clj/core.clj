53764e3a1d46edc8ecda066907677713c789e295
tropicbliss <tropicbliss@protonmail.com>
2025-07-07 23:55:05 +0800

    Reformat code

--- src/rssgossip_clj/core.clj
+++ src/rssgossip_clj/core.clj
@@ -64,12 +64,13 @@
       (let [rss-feed (System/getenv "RSS_FEED") urls (split rss-feed)]
         (doseq [url urls]
           (let [response (http/get url)]
-            (try (let [dom (xml/parse-str (:body response))]
-                   (doseq [node (xml-seq dom)]
-                     (if (= :item (:tag node))
-                       (let [title-node (first-child-by-name node :title) link-node (first-child-by-name node :link) txt (remove-accents (get-node-content title-node))]
-                         (when (re-find pattern txt)
-                           (println txt)
-                           (if include-urls
-                             (println (str "\t" (get-node-content link-node)))))))))
-                 (catch Exception _ (System/exit 1)))))))))
+            (try
+              (let [dom (xml/parse-str (:body response))]
+                (doseq [node (xml-seq dom)]
+                  (if (= :item (:tag node))
+                    (let [title-node (first-child-by-name node :title) link-node (first-child-by-name node :link) txt (remove-accents (get-node-content title-node))]
+                      (when (re-find pattern txt)
+                        (println txt)
+                        (if include-urls
+                          (println (str "\t" (get-node-content link-node)))))))))
+              (catch Exception _ (System/exit 1)))))))))
